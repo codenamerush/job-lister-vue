76f5962cbb9440e9217994a078136194ca78506f
codenamerush <aarushisharda@gmail.com>
2026-01-07 16:24:23 +0530

    directives and loops

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        name: 'John Doe',
+        status: 'inactive',
+        tasks: ['Task 1', 'Task 2', 'Task 3'],
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'inactive'">User is Inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+</template>
+
